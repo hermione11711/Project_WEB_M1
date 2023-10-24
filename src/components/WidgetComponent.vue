62d0fd8f0345c7ab3be2e227a6d1954125bd8f71
hermione11711 <alderthault@hotmail.fr>
2023-10-24 08:47:58 +0200

    CryptoPage

---
+++ src/components/WidgetComponent.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="tradingview-widget-container">
+      <div class="tradingview-widget-container__widget"></div>
+      <div class="tradingview-widget-copyright">
+        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+        </a>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TradingViewWidget',
+    mounted() {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.async = true;
+      script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js';
+      script.text = JSON.stringify({
+        symbols: [
+        {
+      "proName": "FOREXCOM:SPXUSD",
+      "title": "S&P 500"
+    },
+    {
+      "proName": "FOREXCOM:NSXUSD",
+      "title": "US 100"
+    },
+    {
+      "proName": "FX_IDC:EURUSD",
+      "title": "EUR to USD"
+    },
+    {
+      "description": "Tesla",
+      "proName": "NASDAQ:TSLA"
+    },
+    {
+      "description": "Apple",
+      "proName": "NASDAQ:AAPL"
+    },
+    {
+      "description": "Nvidia",
+      "proName": "NASDAQ:NVDA"
+    },
+    {
+      "description": "Netflix",
+      "proName": "NASDAQ:NFLX"
+    },
+    {
+      "description": "Starbucks",
+      "proName": "NASDAQ:SBUX"
+    }
+        ],
+        showSymbolLogo: true,
+        colorTheme: 'dark',
+        isTransparent: true,
+        displayMode: 'compact',
+        locale: 'en',
+      });
+      this.$el.appendChild(script);
+    },
+  };
+
+  </script>
+  
+  <style scoped>
+.tradingview-widget-container {
+  margin-left: 20px;
+  width: 97% !important;  
+  height: 100px !important;
+}
+  </style>
+  
